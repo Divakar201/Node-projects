213617c869ad75780dd27ad08ca6c9b1f4d75074
Kareddy <divakar.kareddy@duckcreek.com>
2019-11-24 11:25:35 +0530

    correcting loading of css

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(PublicDirPath));
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        body: 'Home page',
+        body: 'Weather page',
         name: 'Divakar'
     })
 })
